5dc375db6741c2661d3b1bb55ed9693379ec69d6
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-12 21:02:42 +0300

    Create CartConfiguration.cs

---
+++ Infrastructure/Configurations/CartConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class CartConfiguration : IEntityTypeConfiguration<Cart>
+{
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.ToTable("Carts");
+
+        // Key
+        builder.HasKey(x => x.Id);
+
+        // Properties
+        builder.Property(x => x.UserId).IsRequired();
+
+        // Indexes
+        builder.HasIndex(x => x.UserId);
+    }
+}
