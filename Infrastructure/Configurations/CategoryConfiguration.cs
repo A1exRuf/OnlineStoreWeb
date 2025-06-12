d6ca138ae95eda1e7c197647970003200d86e1f4
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-12 21:59:13 +0300

    Create CategoryConfiguration.cs

---
+++ Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        // Key
+        builder.HasKey(x => x.Id);
+
+        // Properties
+        builder.Property(x =>  x.Name).IsRequired();
+        builder.Property(x => x.ParentCategoryId);
+
+        // Indexes
+        builder.HasIndex(x => x.ParentCategoryId);
+    }
+}
