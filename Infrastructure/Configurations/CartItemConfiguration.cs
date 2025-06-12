b284b53a0107aa721d874d27a65d3add0a0364f0
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-12 21:17:11 +0300

    Create CartItemConfiguration.cs

---
+++ Infrastructure/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    internal class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.ToTable("CartItems");
+
+            // Key
+            builder.HasKey(x => x.Id);
+
+            // Properties
+            builder.Property(x => x.CartId).IsRequired();
+            builder.Property(x => x.ProductId).IsRequired();
+            builder.Property(x => x.Quantity).IsRequired().HasAnnotation("MinValue", 1);
+
+            // Indexes
+            builder.HasIndex(x => x.CartId);
+        }
+    }
+}
