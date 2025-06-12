13c6d181401446d519f8f36bee97aeacc251e6a7
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-12 23:00:49 +0300

    Create OrderConfiguration.cs

---
+++ Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+
+        // Key
+        builder.HasKey(x => x.Id);
+
+        // Properties
+        builder.Property(x => x.UserId).IsRequired();
+        builder.Property(x => x.OrderDate).IsRequired();
+        builder.Property(x => x.Status).IsRequired().HasConversion<string>().HasMaxLength(20);
+        builder.Property(x => x.PaidDate);
+        builder.Property(x => x.ShippingDate);
+        builder.Property(x => x.CompletedDate);
+        builder.Property(x => x.Address).IsRequired().HasMaxLength(160);
+
+        // Relations
+        builder.HasOne(o => o.User)
+            .WithMany(u => u.Orders)
+            .HasForeignKey(o => o.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasMany(o => o.Items)
+            .WithOne(oi => oi.Order)
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Indexes
+        builder.HasIndex(x => x.UserId);
+    }
+}
