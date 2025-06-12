0c03139f1983e34e2876f02de8c449ccc527583a
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-12 23:23:17 +0300

    Create OrderItemConfiguration.cs

---
+++ Infrastructure/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    internal class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            // Key
+            builder.HasKey(x => x.Id);
+
+            // Properties
+            builder.Property(x => x.OrderId).IsRequired();
+            builder.Property(x => x.ProductId).IsRequired();
+            builder.Property(x => x.Quantity).IsRequired().HasAnnotation("MinValue", 1);
+            builder.Property(x => x.UnitPrice).IsRequired();
+
+            // Relations
+            builder.HasOne(oi => oi.Order)
+                .WithMany(o => o.Items)
+                .HasForeignKey(oi => oi.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(oi => oi.Product)
+                .WithMany()
+                .HasForeignKey(oi => oi.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Indexes
+            builder.HasIndex(x => x.OrderId);
+        }
+    }
+}
