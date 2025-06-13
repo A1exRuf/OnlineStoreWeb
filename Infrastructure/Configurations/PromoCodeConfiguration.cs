26405147c1a70768c5509c1a153e7bbfa137e87c
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-13 11:48:14 +0300

    Create PromoCodeConfiguration.cs

---
+++ Infrastructure/Configurations/PromoCodeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    internal class PromoCodeConfiguration : IEntityTypeConfiguration<PromoCode>
+    {
+        public void Configure(EntityTypeBuilder<PromoCode> builder)
+        {
+            builder.ToTable("PromoCodes");
+
+            // Key
+            builder.HasKey(pc => pc.Id);
+
+            // Properties
+            builder.Property(pc => pc.Code).IsRequired().HasMaxLength(40);
+            builder.Property(pc => pc.DiscountValue).IsRequired();
+            builder.Property(pc => pc.ExpiryDate).IsRequired();
+            builder.Property(pc => pc.IsActive).IsRequired();
+
+            // Indexes
+            builder.HasIndex(pc => pc.Code);
+        }
+    }
+}
