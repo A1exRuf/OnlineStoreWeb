aff1140184665d5444bd57610ca4dfca52e84253
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-13 20:06:50 +0300

    Abstractions
    Added Repository, Filter, UnitOfWork interfaces and PagedList

---
+++ Domain/Abstractions/IFilter.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions;
+
+public interface IFilter<TEntity> where TEntity : Entity
+{
+    IQueryable<TEntity> ApplyFilter(IQueryable<TEntity> query);
+}
---
+++ Domain/Abstractions/IRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Domain.Abstractions;
+
+public interface IRepository<TEntity> where TEntity : Entity
+{
+    // Add
+    Task AddAsync(
+        TEntity entity, 
+        CancellationToken cancellationToken = default);
+
+    // Remove
+    Task<bool> RemoveByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    // Update
+    void Update(TEntity entity);
+
+    // Exists
+    Task<bool> ExistsAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    // Get
+    Task<TEntity?> GetAsync(
+        IFilter<TEntity> filter,
+        bool asNoTracking = true,
+        CancellationToken cancellationToken = default);
+    Task<TDto?> GetAsync<TDto>(
+        IFilter<TEntity> filter,
+        bool asNoTracking = true,
+        CancellationToken cancellationToken = default);
+
+    // Get List
+    Task<List<TDto>> GetListAsync<TDto, TKey>(
+        IFilter<TEntity> filter,
+        bool asNoTracking = true,
+        Expression<Func<TEntity, TKey>>? orderBy = null,
+        bool descending = false,
+        CancellationToken cancellationToken = default);
+
+    // Get Paged List
+    Task<PagedList<TDto>> GetPagedListAsync<TDto, TKey>(
+        int page,
+        int pageSize,
+        IFilter<TEntity> filter,
+        bool asNoTracking = true,
+        Expression<Func<TEntity, TKey>>? orderBy = null,
+        bool descending = false,
+        CancellationToken cancellationToken = default);
+}
