a79e78789a719a2dcf3b985ef2fc9348671abefe
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-13 11:03:11 +0300

    Create ProductImageConfiguration.cs

---
+++ Infrastructure/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+{
+    public void Configure(EntityTypeBuilder<ProductImage> builder)
+    {
+        builder.ToTable("ProductImages");
+
+        // Key
+        builder.HasKey(pi => pi.Id);
+
+        // Properties
+        builder.Property(pi => pi.ProductId).IsRequired();
+        builder.Property(pi => pi.ImageUrl).IsRequired().HasMaxLength(50);
+        builder.Property(pi => pi.AltText).HasMaxLength(20);
+        builder.Property(pi => pi.DisplayOrder).IsRequired();
+
+        // Relations
+        builder.HasOne(pi => pi.Product)
+            .WithMany(p => p.Images)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        // Indexes
+        builder.HasIndex(pi => pi.ProductId);
+    }
+}
