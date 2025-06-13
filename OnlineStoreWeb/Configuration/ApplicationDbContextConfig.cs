d2055e7bd15cb002cc88f538e3c1f59968a50615
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-13 19:14:49 +0300

    DI registration and configuration

---
+++ OnlineStoreWeb/Configuration/ApplicationDbContextConfig.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineStoreWeb.Configuration
+{
+    public static class ApplicationDbContextConfig
+    {
+        public static void AddApplicationDbContextConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(builder =>
+            builder.UseNpgsql(configuration.GetConnectionString("Database")));
+        }
+    }
+}
--- OnlineStoreWeb/Program.cs
+++ OnlineStoreWeb/Program.cs
@@ -1,8 +1,10 @@
+using OnlineStoreWeb.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationDbContextConfig(builder.Configuration);
 
 var app = builder.Build();
 
