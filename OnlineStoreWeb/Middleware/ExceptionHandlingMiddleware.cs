19998eb02a3859369c70b09d86999c3d0901cf12
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-14 15:01:16 +0300

    feat: added exception handling middleware

---
+++ OnlineStoreWeb/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Exceptions.Base;
+using System.Text.Json;
+
+namespace OnlineStoreWeb.Middleware;
+
+internal class ExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+    {
+        httpContext.Response.ContentType = "application/json";
+        httpContext.Response.StatusCode = exception switch
+        {
+            BadRequestException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var response = new
+        {
+            status = httpContext.Response.StatusCode,
+            message = exception.Message,
+        };
+
+        await httpContext.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
