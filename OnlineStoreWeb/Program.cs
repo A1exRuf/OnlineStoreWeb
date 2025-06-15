c3457b71ca39df4f31b4af6750ede4903258ddd7
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-15 19:20:52 +0300

    Dependency Injection

--- OnlineStoreWeb/Program.cs
+++ OnlineStoreWeb/Program.cs
@@ -1,11 +1,15 @@
-using OnlineStoreWeb.Configuration;
 using OnlineStoreWeb.Middleware;
+using Application;
+using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
-builder.Services.AddApplicationDbContextConfig(builder.Configuration);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 
 var app = builder.Build();
 
