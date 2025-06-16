a024e7cf5b7b6d09cab4600d10e62d0a9eb75186
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-16 10:55:47 +0300

    Registration

---
+++ Application/Filters/UserFilter.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+
+namespace Application.Filters
+{
+    public class UserFilter : IFilter<User>
+    {
+        public Guid? Guid { get; set; }
+        public string? Email { get; set; }
+
+        public IQueryable<User> ApplyFilter(IQueryable<User> query)
+        {
+            if (Guid.HasValue)
+            {
+                query = query.Where(x => x.Id == Guid.Value);
+
+                return query;
+            }
+
+            if (!string.IsNullOrEmpty(Email))
+            {
+                query = query.Where(x => x.Email == Email);
+            }
+
+            return query;
+        }
+    }
+}
---
+++ Application/UseCases/Users/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Abstractions;
+using Application.Abstractions.Messaging;
+using Domain.Abstractions;
+using Domain.Common;
+using Domain.Entities;
+
+namespace Application.UseCases.Users.Commands.Register;
+
+public class RegisterCommandHandler : ICommandHandler<RegisterCommand, Guid>
+{
+    private readonly IRepository<User> _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public RegisterCommandHandler(
+        IRepository<User> userRepository, 
+        IUnitOfWork unitOfWork, 
+        IPasswordHasher passwordHasher)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<Guid> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        var hashedPassword = _passwordHasher.HashPassword(request.Password);
+
+        var user = new User(
+            Guid.NewGuid(), 
+            request.Email, 
+            hashedPassword, 
+            Enum.Parse<UserRole>(request.Role, true));
+
+        await _userRepository.AddAsync(user, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return user.Id;
+    }
+}
