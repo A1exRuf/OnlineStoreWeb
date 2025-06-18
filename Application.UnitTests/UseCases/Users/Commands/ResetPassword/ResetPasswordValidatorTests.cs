c217af5c92ac3be18a783d2223e8c65812893692
AlexRuf <AlexeyRufeev@gmail.com>
2025-06-18 20:11:56 +0300

    test: reset-password

---
+++ Application.UnitTests/UseCases/Users/Commands/ResetPassword/ResetPasswordValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using Application.UseCases.Users.Commands.ResetPassword;
+using FluentValidation;
+using FluentValidation.TestHelper;
+
+namespace Application.UnitTests.UseCases.Users.Commands.ResetPassword;
+
+public class ResetPasswordValidatorTests : AbstractValidator<ResetPasswordCommand>
+{
+    private readonly ResetPasswordValidator _validator;
+
+    public ResetPasswordValidatorTests()
+    {
+        _validator = new ResetPasswordValidator();
+    }
+
+    [Theory]
+    [InlineData("", "Email is required")]
+    [InlineData("invalid-email", "Invalid email format")]
+    public async Task Should_HaveValidationError_When_EmailIsInvalid(
+        string email,
+        string expectedErrorMessage)
+    {
+        // Arrange
+        var command = new ResetPasswordCommand(email, "123456", "password", "password");
+
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Email).WithErrorMessage(expectedErrorMessage);
+    }
+
+    [Fact]
+    public async Task Should_HaveValidationError_When_ResetTokenIsEmpty()
+    {
+        // Arrange
+        var command = new ResetPasswordCommand("email@test.com", "", "123456", "123456");
+
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ResetToken).WithErrorMessage("Reset token is requered");
+    }
+
+    [Theory]
+    [InlineData("", "Password is required")]
+    [InlineData("1234567", "Password must be at least 8 characters long")]
+    public async Task Should_HaveValidationError_When_PasswordIsInvalid(string password, string expectedErrorMessage)
+    {
+        // Arrange
+        var command = new ResetPasswordCommand("email@test.com", "123456", password, password);
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Password).WithErrorMessage(expectedErrorMessage);
+    }
+
+    [Fact]
+    public async Task Should_HaveValidationError_When_ConfirmPasswordIsInvalid()
+    {
+        // Arrange
+        var command = new ResetPasswordCommand("email@test.com", "123456", "password", "12345678");
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ConfirmPassword).WithErrorMessage("Passwords do not match");
+    }
+
+
+}
